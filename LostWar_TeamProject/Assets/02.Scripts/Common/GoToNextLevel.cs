b3eb7d0939d29420e9e0131cbce14ba1a74e5ff6
JaeSung Lee <wotjd999@gmail.com>
2022-08-05 15:27:31 +0900

    씬 전환 조건 수정
    다음 레벨 진행 시 퀘스트가 완수되지 않으면
    전환되지 않게 수정.

--- LostWar_TeamProject/Assets/02.Scripts/Common/GoToNextLevel.cs
+++ LostWar_TeamProject/Assets/02.Scripts/Common/GoToNextLevel.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class GoToNextLevel : MonoBehaviour
 {
     [SerializeField] private string transferMapName;
-
+    [SerializeField] private int questId;
+    private string alert;
     private PlayerInteraction thePlayer;
 
     private void Start()
     {
         thePlayer = FindObjectOfType<PlayerInteraction>();
+        alert = "퀘스트를 더 진행해야 합니다.";
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            if (QuestManager.instance.QuestId < questId)
+            {
+                StartCoroutine(UIManager.instance.NoticeText(false, alert));
+                return;
+            }
             thePlayer.currentMapName = transferMapName;
             SceneLoader.LoadScene(transferMapName);
         }
